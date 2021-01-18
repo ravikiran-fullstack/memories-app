6f66b60363aa2387f31ec5e3ccdd4bb87855d52c
kiran-code <ravikiransjce.code@gmail.com>
2021-01-18 20:00:02 +0530

    Prepared project for deployment in heroku

--- server.js
+++ server.js
@@ -2,19 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/post.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.send('Welcome to Postcard API');
+});
+
 app.use('/posts', postRoutes);
 
-const uri = "mongodb+srv://mem-user:test123@urlshortener.6jhak.mongodb.net/post-memories?retryWrites=true&w=majority";
+const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
